eda4dac7a8c734213a262bbf1080a83dc8b3fde3
liangyuan <leberco@sina.com>
2021-12-22 17:46:00 +0800

    Initial commit

---
+++ src/test/java/com/ly/report/reports/ReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly.report.reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
